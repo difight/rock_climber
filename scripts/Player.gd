d9cb68927635fbac0fe1fc4fab565395eed95c4e
difight <greed_nord@mail.ru>
2021-11-02 21:37:18 +0300

    stage 2

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var Right_Hand:RigidBody2D = get_node("Right_Hand")
+onready var Left_Hand:RigidBody2D = get_node("Left_Hand")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -14,10 +15,16 @@ func _process(delta):
 	pass # Replace with function body.
 
 func _on_Go_To_Rock(current):
-	if current.position:
-		print(current.global_position)
-		var target_vector = Vector2(current.global_position)
-		Right_Hand.linear_velocity = target_vector
+	var positionTarget = current.get_position()
+	if positionTarget:
+		var target_vector = Vector2(current.get_position().x, current.get_position().y)
+		var current_hand = find_nearest_hand(positionTarget)
+		Right_Hand.set_axis_velocity(target_vector)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func find_nearest_hand(position:Vector2) -> RigidBody2D :
+	var raz = Right_Hand.get_position() - position
+	#print(raz)
+	return Right_Hand
 #func _process(delta):
 #	pass
